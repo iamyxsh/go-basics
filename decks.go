0de8c93b844e8f536a991bae146228d22c63c2d5
Yash Sharma <yxshsharma@Yashs-MacBook-Air.local>
2021-03-18 00:06:45 +0530

    read-write file

--- decks.go
+++ decks.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt" //importing a package to print statements
+import (
+	"fmt" //importing a package to print statements
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string // declaring decks that extends the property of slice of type strings
 
@@ -23,4 +28,31 @@ func (d deck) print(){ // d is a reciever, same as this in js
 	for i, card := range d{ // every elemnet with type deck can use this func
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) deal(hand int) (deck,deck){ //returns 2 variables with type deck and deck
+	return d[:hand], d[hand:]  //range of slice
+}
+
+func (d deck) toString() (string){
+	return strings.Join(d, ",") //import from package string
+}
+
+func toDeck(s string) deck {
+	return strings.Split(s, ",")
+}
+
+func writeToFile(d deck, fileName string) error { // returns a variable with type error
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666) //import from package ioutil
+} //write something to a file
+// returns an error , error === nil when operation is a success
+
+func readFromFile(fileName string) string {
+	 d, err:= ioutil.ReadFile(fileName)
+	 if err != nil{
+		fmt.Println(err)
+		 os.Exit(1)
+	 }
+
+	 return string(d)
 }
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main // main package should be involved in every file if it needs to run
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	cards:= newCards()
 
-	cards.print()
+	deal, remaining := cards.deal(4)
+
+	err := writeToFile(deal, "deal.txt")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = writeToFile(remaining, "remaining.txt")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	deal = toDeck(readFromFile("deal.txt")) 
+	remaining = toDeck(readFromFile("remaining.txt")) 
+
+	deal.print()
+	remaining.print()
 }
