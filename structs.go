aeeed4322c3be0aa05f782743cb6ae4473a16c1b
Yash Sharma <yxshsharma@Yashs-MacBook-Air.local>
2021-03-21 00:34:20 +0530

    structs pointer

--- structs.go
+++ structs.go
@@ -2,19 +2,20 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
+type contactInfo struct { 
 	email string
 	pincode int
 }
 
-type person struct {
-	firstName string
+type person struct { // defining a perosn (object) type struct
+	firstName string  // name-of-the-field type
 	lastName string
-	contactInfo
+	contactInfo // embedding diff struct in another struct
+	//same as writting contactInfo contactInfo
 }
 
 func main() {
-	yash := person{
+	yash := person{                       // defining yash as a new person
 		firstName: "Yash",
 		lastName: "Sharma",
 		contactInfo : contactInfo {
@@ -23,6 +24,23 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", yash)
+	pointerToYash := &yash  // getting the address of the variable yash 
+							// & gets you the address of the variable in ram
+							// for eg. ->  1001110
+
+	pointerToYash.updateName("Yxsh", "Shxrma") // calling the function using the pointer
+
+	yash.print()
 }
+
+func(p person) print(){       // recieving p as a person
+		fmt.Printf("%+v", p)
+}
+
+func(pointerToPerson *person) updateName( firstName string, lastName string){  // recieving address (pointer) which is of type pointer to person
+		(*pointerToPerson).firstName = firstName  // updating the person who is at the pointer pointerToPerson
+		(*pointerToPerson).lastName = lastName // * returns the value stored at that pointer
+}
+
+
 
